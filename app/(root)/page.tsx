c0d8ca209fce9945a05b4b8a34b1bf056052d733
hemis23 <themischa9@gmail.com>
2024-05-22 13:04:56 +0300

    Home page UI with animated counter and doughnut chart

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,28 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { log } from "console";
+
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Themistoklis" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions "
+          />
+          <TotalBalanceBox
+            totalBanks={1}
+            totalCurrentBalance={1250.34}
+            accounts={[]}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
