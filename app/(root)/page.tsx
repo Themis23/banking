c93dcc330bc1c375f8c4abeaaff5a867575d43a3
hemis23 <themischa9@gmail.com>
2024-05-27 18:17:02 +0300

    fix display of real bank data with account and accountData

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,23 @@
 import HeaderBox from "@/components/HeaderBox";
 import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 
-const Home = async () => {
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
   const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ userId: loggedIn.$id });
+
+  if (!accounts) return;
+
+  const accountsData = accounts?.data;
+
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
+  const account = await getAccount({ appwriteItemId });
+
+  console.log({ accountsData, account });
+
   return (
     <section className="home">
       <div className="home-content">
@@ -12,21 +25,21 @@ const Home = async () => {
           <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.name || "Guest"}
+            user={loggedIn?.firstName || "Guest"}
             subtext="Access and manage your account and transactions "
           />
           <TotalBalanceBox
-            totalBanks={1}
-            totalCurrentBalance={1250.34}
-            accounts={[]}
+            accounts={accountsData}
+            totalBanks={accounts?.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
           />
         </header>
         RECENT TRANSACTIONS
       </div>
       <RightSidebar
         user={loggedIn}
-        transactions={[]}
-        banks={[{ currentBalance: 123.5 }, { currentBalance: 323.5 }]}
+        transactions={accounts?.transactions}
+        banks={accountsData?.slice(0, 2)}
       />
     </section>
   );
---
+++ banking-main/app/(root)/page.tsx
@@ -0,0 +1,35 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
+
+const Home = async () => {
+  const loggedIn = await getLoggedInUser();
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.name || "Guest"}
+            subtext="Access and manage your account and transactions "
+          />
+          <TotalBalanceBox
+            totalBanks={1}
+            totalCurrentBalance={1250.34}
+            accounts={[]}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 323.5 }]}
+      />
+    </section>
+  );
+};
+
+export default Home;
